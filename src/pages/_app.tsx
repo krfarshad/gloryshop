80c87ae72cdbef5ba6ac61b9cb7e37410ff053a4
Farshad <krfarshad@gmail.com>
2022-08-16 15:04:11 +0430

    refactor : create cart component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,25 @@
 import "../styles/globals.css";
-import { Hydrate ,QueryClientProvider, QueryClient ,dehydrate } from "@tanstack/react-query";
+import {
+  Hydrate,
+  QueryClientProvider,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useState } from "react";
-
+import {  useState } from "react";
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  const dehydratedState = dehydrate(queryClient)
-
+  const dehydratedState = dehydrate(queryClient );
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={dehydratedState}>
-          <Component {...pageProps} />
+            <Component {...pageProps} />
           <ReactQueryDevtools initialIsOpen={false} />
         </Hydrate>
       </QueryClientProvider>
     </>
   );
 }
-
 export default MyApp;
